29157627e405a11cc6c9ce240ee96fd3d29ccf53
Villrax <tratsevskiyivan@gmail.com>
2023-03-11 22:56:07 +0300

    Lab 5 new rep class normal

--- lab1/TRPO lab1/src/Main.java
+++ lab1/TRPO lab1/src/Main.java
@@ -6,11 +6,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         RepositoryClass rc = new RepositoryClass();
-        rc.addNewStudent();
-        rc.addNewStudent();
-        System.out.println("Обновление");
-        rc.getStudents();
         Boolean isExit = false;
+        int studentCurrentId = 1;
         Student[] students = new Student[0];
         Student[] studentsExample = {
                 new ContractStudent("Bob","ITP-11",6.1,1,1200),
@@ -30,7 +27,9 @@ public class Main {
             System.out.println("4 - Получить информацию о студентах-платниках");
             System.out.println("5 - Получить информацию о студентах-бюджетниках");
             System.out.println("6 - Получить средний балл по итогам сессии по всем студентам");
-            System.out.println("7 - Закончить работу");
+            System.out.println("7 - Удалить инфомрацию о студенте");
+            System.out.println("8 - Обновить информацию о студенте");
+            System.out.println("9 - Закончить работу");
             Scanner userChoiseScaner = new Scanner(System.in);
             if (userChoiseScaner.hasNextInt()) {
                 int userChoise = userChoiseScaner.nextInt();
@@ -55,29 +54,28 @@ public class Main {
                             } else{
                                 examPassConfirm = false;
                             }
-                            array[students.length] = new ScholarshipStudent(studentName,studentGroup,studentAvgMark,array.length,examPassConfirm);
-                            students = array;
+                            rc.addNewStudent(examPassConfirm,studentName,studentGroup,studentAvgMark,studentCurrentId);
                         }else if(studentType == 2){
                             System.out.println("Введите стоимость оплаты за обучение");
                             Double studentEduCost = studentAddInput.nextDouble();
-                            array[students.length] = new ContractStudent(studentName,studentGroup,studentAvgMark,array.length,studentEduCost);
-                            students = array;
+                            rc.addNewStudent(studentEduCost,studentName,studentGroup,studentAvgMark,studentCurrentId);
                         }
+                        studentCurrentId += 1;
                         break;
                     case 2:
                         Scanner studentSearchInput = new Scanner(System.in);
                         System.out.println("Введите номер студента по списку");
-                        int studentListNumber = studentSearchInput.nextInt()-1;
-                        System.out.println(studentsExample[studentListNumber].getStudentInfo());
+                        int studentListNumber = studentSearchInput.nextInt();
+                        System.out.println(rc.getStudents()[studentListNumber].getStudentInfo());
                         break;
                     case 3:
-                        for (Student student: studentsExample) {
+                        for (Student student: rc.getStudents()) {
                             System.out.println(student.getStudentInfo());
                         }
                         break;
                     case 4:
                         System.out.println("Студенты обучающиеся на платной основе");
-                        for (Student student: studentsExample) {
+                        for (Student student: rc.getStudents()) {
                             if(student instanceof ContractStudent){
                                 System.out.println(student.getStudentInfo());
                             }
@@ -85,16 +83,50 @@ public class Main {
                         break;
                     case 5:
                         System.out.println("Студенты обучающиеся на бюджете");
-                        for (Student student: studentsExample) {
+                        for (Student student: rc.getStudents()) {
                             if(student instanceof ScholarshipStudent){
                                 System.out.println(student.getStudentInfo());
                             }
                         }
                         break;
                     case 6:
-                        System.out.println("Средний балл всех студентов по итогу сессии: " + Student.getAvgMarkOfAllStudents(studentsExample));
+                        System.out.println("Средний балл всех студентов по итогу сессии: " + Student.getAvgMarkOfAllStudents(rc.getStudents()));
                         break;
                     case 7:
+                        Scanner studentDeleteInput = new Scanner(System.in);
+                        System.out.println("Введите номер студента для удаления");
+                        int studentDeleteNumber = studentDeleteInput.nextInt();
+                        rc.removeStudent(studentDeleteNumber);
+                        break;
+                    case 8:
+                        Scanner studentUpdateInput = new Scanner(System.in);
+                        System.out.println("Введите номер студента для обновления");
+                        int studentUpdateNumber = studentUpdateInput.nextInt();
+                        System.out.println("Введите имя студента");
+                        String studentNameNew = studentUpdateInput.next();
+                        System.out.println("Введите группу студента");
+                        String studentGroupNew = studentUpdateInput.next();
+                        System.out.println("Введите средний балл студента");
+                        Double studentAvgMarkNew = studentUpdateInput.nextDouble();
+                        System.out.println("Введите тип студента (1 - на бюджете/2 - платный)");
+                        int studentTypeNew = studentUpdateInput.nextInt();
+                        if(studentTypeNew == 1){
+                            System.out.println("Студент сдал сессию в срок? (1 - да, 2 - нет)");
+                            int examPassTimeNumber = studentUpdateInput.nextInt();
+                            Boolean examPassConfirm;
+                            if(examPassTimeNumber == 1){
+                                examPassConfirm = true;
+                            } else{
+                                examPassConfirm = false;
+                            }
+                            rc.updateStudentInfo(studentUpdateNumber-1,examPassConfirm,studentNameNew,studentGroupNew,studentAvgMarkNew,studentCurrentId);
+                        }else if(studentTypeNew == 2){
+                            System.out.println("Введите стоимость оплаты за обучение");
+                            Double studentEduCost = studentUpdateInput.nextDouble();
+                            rc.updateStudentInfo(studentUpdateNumber-1,studentEduCost,studentNameNew,studentGroupNew,studentAvgMarkNew,studentCurrentId);
+                        }
+                        break;
+                    case 9:
                         isExit = true;
                         break;
                 }
--- lab1/TRPO lab1/src/RepositoryClass.java
+++ lab1/TRPO lab1/src/RepositoryClass.java
@@ -3,68 +3,25 @@ import java.util.Scanner;
 
 public class RepositoryClass {
     private ArrayList<Student> students = new ArrayList<Student>();
-    public void getStudents(){
-        for(Student student : students){
-            System.out.println(student.getStudentInfo());
-        }
+    public Student[] getStudents(){
+        Student[] studs = new Student[students.size()];
+        studs = students.toArray(studs);
+        return  studs;
     }
-    public void addNewStudent(){
-        Scanner studentAddInput = new Scanner(System.in);
-        System.out.println("Введите имя студента");
-        String studentName = studentAddInput.nextLine();
-        System.out.println("Введите группу студента");
-        String studentGroup = studentAddInput.nextLine();
-        System.out.println("Введите средний балл студента");
-        Double studentAvgMark = studentAddInput.nextDouble();
-        System.out.println("Введите тип студента (1 - на бюджете/2 - платный)");
-        int studentType = studentAddInput.nextInt();
-        if(studentType == 1){
-            System.out.println("Студент сдал сессию в срок? (1 - да, 2 - нет)");
-            int examPassTimeNumber = studentAddInput.nextInt();
-            Boolean examPassConfirm;
-            if(examPassTimeNumber == 1)
-                examPassConfirm = true;
-            else
-                examPassConfirm = false;
-            students.add(new ScholarshipStudent(studentName,studentGroup,studentAvgMark, students.size()+1,examPassConfirm));
-        }else if(studentType == 2){
-            System.out.println("Введите стоимость оплаты за обучение");
-            Double studentEduCost = studentAddInput.nextDouble();
-            students.add(new ContractStudent(studentName,studentGroup,studentAvgMark, students.size()+1,studentEduCost));
-        }
+    public void addNewStudent(boolean examPassConfirm, String studentName, String studentGroup, double studentAvgMark, int id){
+        students.add(new ScholarshipStudent(studentName,studentGroup,studentAvgMark, id,examPassConfirm));
+    }
+    public void addNewStudent(double studentEducationCost, String studentName, String studentGroup, double studentAvgMark, int id){
+        students.add(new ContractStudent(studentName,studentGroup,studentAvgMark, id,studentEducationCost));
     }
 
-    /**public void updateStudentInfo() {
-        Scanner updateInput = new Scanner(System.in);
-        System.out.println("Для редактирвоания введите номер студента по списку");
-        int studentListNumber = updateInput.nextInt()-1;
-        System.out.println("Введите имя студента");
-        String studentName = updateInput.next();
-        System.out.println("Введите группу студента");
-        String studentGroup = updateInput.nextLine();
-        System.out.println("Введите средний балл студента");
-        Double studentAvgMark = updateInput.nextDouble();
-        System.out.println("Введите тип студента (1 - на бюджете/2 - платный)");
-        int studentType = updateInput.nextInt();
-        if(studentType == 1){
-            System.out.println("Студент сдал сессию в срок? (1 - да, 2 - нет)");
-            int examPassTimeNumber = updateInput.nextInt();
-            Boolean examPassConfirm;
-            if(examPassTimeNumber == 1)
-                examPassConfirm = true;
-            else
-                examPassConfirm = false;
-            students.add(new ScholarshipStudent(studentName,studentGroup,studentAvgMark, students.size()+1,examPassConfirm));
-        }else if(studentType == 2){
-            System.out.println("Введите стоимость оплаты за обучение");
-            Double studentEduCost = updateInput.nextDouble();
-            students.add(new ContractStudent(studentName,studentGroup,studentAvgMark, students.size()+1,studentEduCost));
-        }
-    }**/
-    public void removeStudent(){
-        Scanner deleteInput = new Scanner(System.in);
-        System.out.println("Для удаления введите номер студента по списку");
-        int studentListNumber = deleteInput.nextInt();
-        students.remove(studentListNumber-1);
+    public void updateStudentInfo(int changeId,boolean examPassConfirm, String studentName, String studentGroup, double studentAvgMark, int id) {
+        students.set(changeId, new ScholarshipStudent(studentName, studentGroup, studentAvgMark, id, examPassConfirm));
+    }
+    public void updateStudentInfo(int changeId,double studentEducationCost, String studentName, String studentGroup, double studentAvgMark, int id) {
+        students.set(changeId, new ContractStudent(studentName,studentGroup,studentAvgMark, id,studentEducationCost));
+    }
+    public void removeStudent(int id){
+        students.remove(id-1);
     }
 }
